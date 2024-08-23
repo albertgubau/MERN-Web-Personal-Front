1c259b415fa4979410d57e18d0f189734a385050
albertgubau <agubau11@gmail.com>
2024-08-23 12:54:38 +0200

    Creating all the pages and adding their routes

--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Auth, Users } from "../pages/admin"; // With the index.js in admin folder, notice that we can access all the pages only importing the folder
+import { Auth, Users, Blog, Courses, Menu, Newsletter } from "../pages/admin"; // With the index.js in admin folder, notice that we can access all the pages only importing the folder
 import { AdminLayout } from "../layouts";
 
 const user = { username: "admin", password: "admin" };
@@ -19,7 +19,24 @@ export function AdminRouter() {
         <Route path="/admin/*" element={loadLayout(AdminLayout, Auth)} />
       ) : (
         <>
+          {["/admin", "/admin/blog"].map((path) => (
+            <Route
+              key={path}
+              path={path}
+              element={loadLayout(AdminLayout, Blog)}
+            />
+          ))}
+
           <Route path="/admin/users" element={loadLayout(AdminLayout, Users)} />
+          <Route
+            path="/admin/courses"
+            element={loadLayout(AdminLayout, Courses)}
+          />
+          <Route path="/admin/menu" element={loadLayout(AdminLayout, Menu)} />
+          <Route
+            path="/admin/newsletter"
+            element={loadLayout(AdminLayout, Newsletter)}
+          />
         </>
       )}
     </Routes>
