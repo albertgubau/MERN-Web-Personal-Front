d9e289a202eded77a43e2c66ae1247d7c937d322
albertgubau <agubau11@gmail.com>
2024-09-02 16:45:34 +0200

    Admin menu and logout

--- src/layouts/AdminLayout/AdminLayout.js
+++ src/layouts/AdminLayout/AdminLayout.js
@@ -1,10 +1,22 @@
+import "./AdminLayout.scss";
+import { Icon } from "../../assets";
+import { AdminMenu, Logout } from "../../components/Admin/AdminLayout";
+
 export function AdminLayout(props) {
   const { children } = props;
 
   return (
-    <div>
-      <h1>Admin Layout</h1>
-      {children}
+    <div className="admin-layout">
+      <div className="admin-layout__left">
+        <Icon.LogoWhite className="logo" />
+        <AdminMenu />
+      </div>
+      <div className="admin-layout__right">
+        <div className="admin-layout__right-header">
+          <Logout />
+        </div>
+        <div className="admin-layout__right-content">{children}</div>
+      </div>
     </div>
   );
 }
